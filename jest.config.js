4f794fc5fe0de31c34b1e40ce833ecc32c628489
shalldie <hi.xieshuang@gmail.com>
2020-01-06 01:51:01 +0800

    init

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line
+module.exports = {
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: 'src',
+    testRegex: '.spec.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    coverageDirectory: '../coverage',
+    testEnvironment: 'node',
+};
